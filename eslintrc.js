5b67489bc64fdcc31e4604a15196b7f36474b2d5
DayTroy <shadowyguy5@gmail.com>
2023-10-04 10:55:11 +0300

    Added TypeScript

---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+      // Add specific rules or overrides here
+    },
+  };
+  
